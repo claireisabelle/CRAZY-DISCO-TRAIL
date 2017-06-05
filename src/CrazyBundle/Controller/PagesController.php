cd643e5070a14962e9a0924839549300838e3fe7
claireisabelle <claire.bourdale@gmail.com>
2017-06-05 14:17:10 +0200

    Mise en place template

---
+++ src/CrazyBundle/Controller/PagesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CrazyBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class PagesController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('CrazyBundle:Pagesinternes:index.html.twig');
+    }
+}
---
+++ src/CrazyBundle/CrazyBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CrazyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CrazyBundle extends Bundle
+{
+}
